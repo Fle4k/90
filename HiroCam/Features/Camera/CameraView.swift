f9fbccad6709b6f71d4c673e92e288e232412931
Shahin shokoui <shahin@shahin.de>
2025-09-07 00:05:27 +0200

    v1.0

--- HiroCam/Features/Camera/CameraView.swift
+++ HiroCam/Features/Camera/CameraView.swift
@@ -671,7 +671,7 @@ struct ThemedSettingsSheetView: View {
                 .fontWeight(.medium)
                 .foregroundColor(settingsPrimaryTextColor)
             
-            Text("0.5.0")
+            Text("1.0")
                 .font(.caption)
                 .foregroundColor(settingsSecondaryTextColor)
         }
